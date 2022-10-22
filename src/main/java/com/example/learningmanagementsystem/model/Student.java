f664aa484597a74b81c7d517a8b2559592542c15
Dulnath <kumuthudulnath@gmail.com>
2022-10-22 16:37:40 +0530

    integrated manual id input system

--- src/main/java/com/example/learningmanagementsystem/model/Student.java
+++ src/main/java/com/example/learningmanagementsystem/model/Student.java
@@ -16,16 +16,14 @@ import javax.persistence.GenerationType;
 @Setter
 @ToString
 @Document(collection="Student")
-@Entity
 public class Student {
-    @Id
     private int id;
     private String firstName;
     private String lastName;
     private String dateOfBirth;
     private String address;
 
-    public Student(int  id, String firstName, String lastName, String dateOfBirth, String address) {
+    public Student(int id, String firstName, String lastName, String dateOfBirth, String address) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
