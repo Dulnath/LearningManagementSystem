908a7761712e0d331f987757a9866d8dc1155b89
Samith-Ailapperuma <samithailapperuma@gmail.com>
2022-10-23 22:34:25 +0530

    Add comments and rerieve comments

--- src/main/java/com/example/learningmanagementsystem/controller/DiscussionForumController.java
+++ src/main/java/com/example/learningmanagementsystem/controller/DiscussionForumController.java
@@ -1,9 +1,46 @@
 package com.example.learningmanagementsystem.controller;
 
+import com.example.learningmanagementsystem.model.DiscussionForum;
+import com.example.learningmanagementsystem.repository.DiscussionForumRepository;
+import com.example.learningmanagementsystem.services.SequenceGeneratorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class DiscussionForumController {
 
+    @Autowired
+    private DiscussionForumRepository discussionForumRepository;
 
+    @Autowired
+    private SequenceGeneratorService sequenceGeneratorService;
+
+
+
+    // Functionality of save button
+    @RequestMapping (value="/saveComment", method=RequestMethod.POST)
+    public String saveComment(@ModelAttribute("discussionForum") DiscussionForum df){
+        df.setId(sequenceGeneratorService.generateSequence(DiscussionForum.SEQUENCE_NAME));
+        discussionForumRepository.save(df);
+        return "redirect:/allComments";
+    }
+
+    // Add comment route
+    @GetMapping("/addComment")
+    public String addComment(Model model){
+        model.addAttribute("comment",new DiscussionForum());
+        return "addComment";
+    }
+
+    // Get all comments
+    @GetMapping("/allComments")
+    public String getComments(Model model){
+        List<DiscussionForum>  listForum = discussionForumRepository.findAll();
+        model.addAttribute("listForum",listForum);
+        return "discussionForum";
+    }
 }
--- src/main/java/com/example/learningmanagementsystem/model/DiscussionForum.java
+++ src/main/java/com/example/learningmanagementsystem/model/DiscussionForum.java
@@ -2,32 +2,36 @@ package com.example.learningmanagementsystem.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
+@ToString
+@Document(collection = "Discussion_Forum")
 public class DiscussionForum {
-    private int commentId;
-    private int studentId;
-    private String studentFirstName;
-    private String studentLastName;
-    private int teacherId;
-    private String teacherFirstName;
-    private String teacherLastName;
-    private int responseTo;
+    @Transient
+    public static final String SEQUENCE_NAME = "users_sequence";
+    private long id;
+    //private String studentFirstName;
+    //private String studentLastName;
+    //private int teacherId;
+    //private String teacherFirstName;
+    //private String teacherLastName;
+    //private int responseTo;
     private String title;
     private String comment;
 
-    public DiscussionForum(int commentId, int studentId, String studentFirstName, String studentLastName, int teacherId, String teacherFirstName, String teacherLastName, int responseTo, String title, String comment){
-        this.commentId = commentId;
-        this.studentId = studentId;
-        this.studentFirstName = studentFirstName;
-        this.studentLastName = studentLastName;
-        this.teacherId = teacherId;
-        this.teacherFirstName = teacherFirstName;
-        this.teacherLastName = teacherLastName;
-        this.responseTo = responseTo;
+    public DiscussionForum(String title, String comment){
+        this.id = id;
         this.title = title;
         this.comment = comment;
+    }
+
+    public DiscussionForum(){
+
     }
 
 }
