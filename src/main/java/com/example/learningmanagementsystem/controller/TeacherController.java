da83e207f6e0eb3d8be702f0fec4d4827a57d907
amashali <desilvanas.19@uom.lk>
2022-10-31 23:51:24 +0530

    resolved Done

--- src/main/java/com/example/learningmanagementsystem/controller/TeacherController.java
+++ src/main/java/com/example/learningmanagementsystem/controller/TeacherController.java
@@ -1,7 +1,9 @@
 package com.example.learningmanagementsystem.controller;
 
 
+import com.example.learningmanagementsystem.model.Student;
 import com.example.learningmanagementsystem.model.Teacher;
+import com.example.learningmanagementsystem.repository.StudentRepository;
 import com.example.learningmanagementsystem.repository.TeacherRepository;
 import com.example.learningmanagementsystem.services.SequenceGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,31 +20,35 @@ public class TeacherController {
     @Autowired
     private TeacherRepository teacherRepository;
 
+    @Autowired
+    private SequenceGeneratorService sequenceGeneratorService;
+
     @RequestMapping(value = "/saveTeacher", method = RequestMethod.POST)
-    public String saveTeacher(@ModelAttribute("teacher") Teacher teach){
-        teacherRepository.save(teach);
+    public String saveTeacher(@ModelAttribute("teacher") Teacher tch){
+        //std.setId(sequenceGeneratorService.generateSequence(Student.SEQUENCE_NAME));
+        teacherRepository.save(tch);
         return "redirect:/allTeachers";
     }
 
     @RequestMapping(value = "/saveExistingTeacher", method = RequestMethod.POST)
-    public String saveExistingTeacher(@ModelAttribute("teacher") Teacher teacher){
+    public String saveExistingTeacher(@ModelAttribute("teacher") Teacher tch){
         System.out.println("Working");
-        teacherRepository.save(teacher);
+        teacherRepository.save(tch);
         return "editteacher";
     }
 
     @RequestMapping(value="/editTeacher/{id}")
     public ModelAndView showEditTeacherPage(@PathVariable(name = "id") String id) {
-        ModelAndView mav = new ModelAndView("editteacher");
-        Optional<Teacher> teach = teacherRepository.findById(id);
-        mav.addObject("teacher", teach);
+        ModelAndView mav = new ModelAndView("edittaecher");
+        Optional<Teacher> tch = teacherRepository.findById(id);
+        mav.addObject("teacher", tch);
         return mav;
 
     }
     @GetMapping("/allTeachers")
     public String getTeachers(Model model){
-        List<Teacher> listteacher = teacherRepository.findAll();
-        model.addAttribute("listteacher",listteacher);
+        List<Teacher>  listteacher = teacherRepository.findAll();
+        model.addAttribute("liststudent",listteacher);
         return "allteachers";
     }
 
